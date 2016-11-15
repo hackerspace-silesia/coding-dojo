07e14c61099b73c9a4981bb40fc953324d36b095
Firemark <marpiechula@gmail.com>
2016-11-15 21:00:29 +0100

    coding dojo katopy #8

--- pacman/main.py
+++ pacman/main.py
@@ -1,11 +1,90 @@
 from logic import BaseGame
 
+class Player(object):
+    LEFT = 0
+    RIGHT = 1
+    UP = 2
+    DOWN = 3
+    DIRECTIONS = set([LEFT, RIGHT, UP, DOWN])
+
+    def __init__(self):
+        self.position = [0, 0]
+        self.movement_direction = None
+
+    def get_position(self):
+        return self.position
+
+    def set_direction(self, direction):
+        if direction not in self.DIRECTIONS:
+            raise ValueError('wrong direction')
+        self.movement_direction = direction
+
+    def move(self):
+        if self.movement_direction == self.LEFT:
+            self.position[0] -= 1
+        elif self.movement_direction == self.RIGHT:
+            self.position[0] += 1
+        elif self.movement_direction == self.UP:
+            self.position[1] -= 1
+        elif self.movement_direction == self.DOWN:
+            self.position[1] += 1
+
+
+class Pacman(Player):
+
+    def __init__(self):
+        self.position = (15, 15)
+        self.movement_direction = None
+        self.live = 3
+
+    def get_life_points(self):
+        return self.live
+
+
+class PacmanMap(object):
+    MAX_X = 20
+    MAX_Y = 32
+
+    WALL = 0
+    CANDY = 1
+
+    def __init__(self):
+        self.storage = [
+            [None for x in range(self.MAX_X)] 
+            for y in range(self.MAX_Y)]
+
+    def add_object(self, x, y, obj):
+        if ( x >= 0 and x < self.MAX_X and
+            y >= 0 and y < self.MAX_Y ):
+            self.storage[y][x] = obj
+        else:
+            raise IndexError
+
+    def get_object(self, x, y):
+        return self.storage[y][x]
+
+    def lets_build_a_wall(self):
+        for x in range(self.MAX_X):
+            self.add_object(x, 0, 0)
+        for y in range(self.MAX_Y):
+            self.add_object(0, y, 0)
+
+
+
+
+    # def _get_line(self, y):
+    #     try:
+    #         line = self.storage[y]
+    #     except IndexError:
+    #         line = []
+    #         self.storage[y] = line
+    #     return line
 
 class Game(BaseGame):
-    t = 0
 
     def init(self):
-        pass
+        self.pmap = PacmanMap()
+        self.pacman = Pacman()
 
     def loop(self, key):
         if key == 'w':
@@ -19,7 +98,6 @@ class Game(BaseGame):
         # self.draw_point(x, y)
         # self.draw_ghost(x, y)
         # self.draw_text(x, y, 'string')
-        self.t = 1 - self.t
         self.draw_pacman(0, 0)
         self.draw_wall(1, 1)  # ▓
         self.draw_ghost(2, 2) # ☢
--- pacman/test.py
+++ pacman/test.py
@@ -1,4 +1,119 @@
 from main import Game
+from main import PacmanMap
+from main import Pacman, Player
+import pytest
+from copy import copy
 
 def test_x():
-    assert 0 == 0
+    assert PacmanMap() is not None
+
+def test_adding_object_to_map():
+    pmap = PacmanMap()
+    pmap.add_object(1, 1, None)
+
+    assert pmap.get_object(1, 1) is None
+
+def test_adding_object_to_map_second():
+    pmap = PacmanMap()
+    pmap.add_object(0, 0, 1)
+    with pytest.raises(IndexError):
+        pmap.add_object(31, 20, 1)
+        pmap.add_object(32, 0, 1)
+        assert pmap.get_object(31, 20) == 1
+        assert pmap.get_object(32, 0) == 1
+
+    assert pmap.get_object(0, 0) == 1
+
+def test_get_map_element_when_empty():
+    pmap = PacmanMap()
+    assert pmap.get_object(0,0) == None
+
+def test_map_has_boundries():
+    pmap = PacmanMap()
+    pmap.lets_build_a_wall()
+    for x in range(pmap.MAX_X):
+        assert pmap.get_object(x, 0) == 0
+    for x in range(pmap.MAX_X):
+        assert pmap.get_object(0, x) == 0
+    for y in range(pmap.MAX_Y):
+        assert pmap.get_object(y, 0) == 0
+    for y in range(pmap.MAX_Y):
+        assert pmap.get_object(0, y) == 0
+
+
+
+
+def test_pacman_on_starting_positon():
+    pmap = PacmanMap()
+    hero = Pacman()
+    assert hero.get_position() == (15, 15)
+
+def test_pacman_is_alive_on_start():
+    pmap = PacmanMap()
+    hero = Pacman()
+    assert hero.get_life_points()
+
+def test_start_direction_pacman():
+    hero = Pacman()
+    assert hero.movement_direction is None
+
+def test_change_direction():
+    hero = Pacman()
+    hero.set_direction(Pacman.LEFT) 
+    assert hero.movement_direction == Pacman.LEFT
+
+def test_change_wrong_direction():
+    hero = Pacman()
+    with pytest.raises(ValueError):
+        hero.set_direction(-1)
+
+def test_move_player_without_direction():
+    hero = Player()
+    start_position = hero.get_position()
+    hero.move()
+    assert hero.get_position() == start_position
+
+def test_move_player_left():
+    hero = Player()
+    hero.position = [1, 1]
+    start_position = copy(hero.get_position())
+    hero.set_direction(Player.LEFT)
+    hero.move()
+    start_position[0] -= 1
+    assert hero.get_position() == start_position
+
+def test_move_player_right():
+    hero = Player()
+    hero.position = [1, 1]
+    start_position = copy(hero.get_position())
+    hero.set_direction(Player.RIGHT)
+    hero.move()
+    start_position[0] += 1
+    assert hero.get_position() == start_position
+
+def test_move_player_up():
+    hero = Player()
+    hero.position = [1, 1]
+    start_position = copy(hero.get_position())
+    hero.set_direction(Player.UP)
+    hero.move()
+    start_position[1] -= 1
+    assert hero.get_position() == start_position
+
+def test_move_player_down():
+    hero = Player()
+    hero.position = [1, 1]
+    start_position = copy(hero.get_position())
+    hero.set_direction(Player.DOWN)
+    hero.move()
+    start_position[1] += 1
+    assert hero.get_position() == start_position
+
+def test_is_game_initialised():
+    game = Game()
+    game.init()
+    assert game.pacman is not None
+    assert game.pmap is not None
+
+def test_is_move_valid_against_wall():
+    pass
